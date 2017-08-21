6023554b71a53e76802327bc233fc386981ef27c
Guenther Sehn <gtsehn@gmail.com>
2017-08-20 23:25:47 -0300

    Turning db user/pwd into cmd params

--- server/server.js
+++ server/server.js
@@ -1,20 +1,32 @@
 const mongoose = require('mongoose');
+const argv = require('yargs')
+	.option('user', {
+		alias: 'u',
+		required: true
+	})
+	.option('password', {
+		alias: 'p',
+		required: true
+	})
+	.option('database', {
+		alias: 'd',
+		default: 'TodoApp'
+	})
+	.argv;
+
+console.log(argv);
 
 mongoose.Promise = global.Promise;
 
-const password = `h0st1ng!`;
-const database = `TodoApp`;
-const mongodbOriginalURL = `mongodb://admin:<PASSWORD>@cluster0-shard-00-00-ukobs.mongodb.net:27017,cluster0-shard-00-01-ukobs.mongodb.net:27017,cluster0-shard-00-02-ukobs.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
-const mongodbURL = mongodbOriginalURL.replace(/<PASSWORD>/g, password).replace(/<DATABASE>/g, database);
-//const mongodbURL = 'mongodb://localhost:27017/TodoApp';
+const {user, password, database} = argv;
 
-console.log(mongodbURL);
+const mongodbOriginalURL = `mongodb://<USER>:<PASSWORD>@cluster0-shard-00-00-ukobs.mongodb.net:27017,cluster0-shard-00-01-ukobs.mongodb.net:27017,cluster0-shard-00-02-ukobs.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
+const mongodbURL = mongodbOriginalURL.replace(/<USER>/g, user).replace(/<PASSWORD>/g, password).replace(/<DATABASE>/g, database);
+//const mongodbURL = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.connect(mongodbURL, { useMongoClient: true })
 .then((db) => {
 	console.log(`Mongoose connected\n`, db);
-	db.close();
-	console.log(`DB closed`);
 })
 .catch((err) => console.log(`Mongoose Error`, err));
 
