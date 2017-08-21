35056127b76a34ed16876cc687ac7320c310a874
Guenther Sehn <gtsehn@gmail.com>
2017-08-20 22:09:11 -0300

    Adding Atlas connection URL

--- server/server.js
+++ server/server.js
@@ -2,8 +2,20 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true })
-.then(() => console.log(`Mongoose connected`))
+const password = `h0st1ng!`;
+const database = `TodoApp`;
+const mongodbOriginalURL = `mongodb://admin:<PASSWORD>@cluster0-shard-00-00-ukobs.mongodb.net:27017,cluster0-shard-00-01-ukobs.mongodb.net:27017,cluster0-shard-00-02-ukobs.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
+const mongodbURL = mongodbOriginalURL.replace(/<PASSWORD>/g, password).replace(/<DATABASE>/g, database);
+//const mongodbURL = 'mongodb://localhost:27017/TodoApp';
+
+console.log(mongodbURL);
+
+mongoose.connect(mongodbURL, { useMongoClient: true })
+.then((db) => {
+	console.log(`Mongoose connected\n`, db);
+	db.close();
+	console.log(`DB closed`);
+})
 .catch((err) => console.log(`Mongoose Error`, err));
 
 const Todo = mongoose.model('Todo', {
