41cc5eec25f1eb9fdd6ba98dec7e546d1f71f03b
Guenther Sehn <guenther.sehn@saqueepague.com.br>
2017-08-25 18:37:49 +0000

    Adding PATCH /todos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
@@ -73,6 +74,30 @@ app.delete('/todos/:id', (req, res) => {
 			res.status(404).send();
 		}
 	})
+	.catch((err) => {
+		res.status(400).send();
+		console.log(err);
+	});
+
+});
+
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	const body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body	}, {new: true})
+	.then((todo) => !todo ? res.status(404).send() : res.send({todo}))
 	.catch((err) => {
 		res.status(400).send();
 		console.log(err);
